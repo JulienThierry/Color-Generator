9fb6a9f1f94563a592b11bc4554da418b0dc08f2
Julien Thierry <julienthierrypro@gmail.com>
2022-08-26 15:49:21 +0200

    js Optimisation

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const body = document.querySelector("body");
 const rgb = document.querySelector("div");
-
-const randomRgb = (number) => {
-  number = Math.floor(Math.random() * 255);
-  return number;
-};
 
 const displayRgb = () => {
   let rgb1, rgb2, rgb3;
-  rgb1 = randomRgb(rgb1);
-  rgb2 = randomRgb(rgb2);
-  rgb3 = randomRgb(rgb3);
+  rgb1 = Math.floor(Math.random() * 255);
+  rgb2 = Math.floor(Math.random() * 255);
+  rgb3 = Math.floor(Math.random() * 255);
 
-  body.style.background = `rgb(${rgb1},${rgb2},${rgb3})`;
+  document.body.style.background = `rgb(${rgb1},${rgb2},${rgb3})`;
   rgb.textContent = `rgb(${rgb1},${rgb2},${rgb3})`;
 };
 
-let interval = setInterval(() => {
+setInterval(() => {
   displayRgb();
 }, 2000);
